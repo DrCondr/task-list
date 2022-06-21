013f207ad1fd01fb29462ae94acd4768cad02589
Jorge Ortuno <mandela_94@hotmail.com>
2022-06-20 21:04:57 -0300

    Agregue Mock de tarea

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from "./task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text:"Terminar modulo de angular",
+        day:'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id:2,
+        text: "Hacer compras",
+        day: "Agosto 5 a las 15:00",
+        reminder: true
+    },
+    {
+        id:3,
+        text: "Investigar sobre lenguajes",
+        day: "agosto 6 a las 9:00",
+        reminder: false
+    }
+]
